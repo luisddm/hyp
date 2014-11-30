5862116bcc5dcb30831c18bfef2683862fcd3b75
Luis de Dios Martín <luisddm@gmail.com>
2014-12-01 00:55:03 +0100

    Fixed menu added

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-Hidrosyplant
-============
+# Hidrosyplant
 
-Instalar Node (http://nodejs.org), el cual incluye el gestor de paquetes npm
+Este es el código del sitio web de [Hidrosyplant](http://hidrosyplant.es/beta).
 
-Instalar Gulp de forma global (sudo npm install -g gulp)
+## Instalación
 
-Instalar Bower de forma global (sudo npm install -g bower)
+Para poder construir la web a partir de este código, es necesario disponer del gestor de paquetes npm, el cual es parte de Node.js. En caso contrario, se puede descargar desde [su sitio web](http://nodejs.org/) e instalar con unos pocos clicks.
 
-bower install
+1. Descargar el código de este repositorio y navegar hasta su directorio desde la consola del sistema.
+2. Instalar Gulp y Bower de forma global: `sudo npm -g install bower gulp`
+3. Instalar las dependencias de Node: `npm install`
+4. Instalar las dependencias de Bower: `bower install`
+5. Lanzar Gulp: `gulp`
 
-Ir al directorio local donde se ha clonado este repositorio y descargar las dependencias de desarrollo que vienen en package.json (npm install)
-
-Ejecutar gulp (gulp) con la configuración de Gulpfile.js
+Se abrirá el navegador web predeterminado con el código "compilado" y minimizado, que será generado en el directorio build.
--- js/script.js
+++ js/script.js
@@ -41,6 +41,9 @@
         $(".obra").eq(hash).trigger("click");
     }
 
+
+    // FORM
+
     $(".modal-content").find(".acepto").on("click", function(){
         $("#condiciones").prop("checked", true);
     });
@@ -77,5 +80,55 @@
 
     $(".error").tooltip();
     $('[data-toggle="tooltip"]').tooltip();
+
+
+
+    // SCROLL
+
+    $(window).scroll(function () {
+      var scrollTop = $(window).scrollTop();
+      if(scrollTop < 20) {
+
+        $(".menu").css("margin-top", 20-scrollTop);
+        $(".menu").css({"background-color": "rgba(0,0,0,0.8)", "border-bottom": "2px solid rgba(188,209,92,0)"});
+        $(".logo").css({"height" : 111});
+        $(".menu").find(".navbar").css({"margin-top": "35px", "margin-bottom": "35px"});
+
+
+      } else if(scrollTop > 20 && scrollTop < 90) {
+
+        $(".menu").css("margin-top", 0);
+        var x = (scrollTop/(-2))+45;
+        $(".menu").find(".navbar").css({"margin-top": x, "margin-bottom": x});
+        var trans = (scrollTop*0.00286)+0.743;
+        var transBorder = (scrollTop-20)*0.01428;
+        $(".menu").css({"background-color": "rgba(0,0,0,"+trans+")", "border-bottom": "2px solid rgba(188,209,92,"+transBorder+")"}); //bcd15c 188 209 92
+        $(".logo").css({"height" : 140-scrollTop});
+
+      } else if(scrollTop > 90){
+
+        $(".menu").find(".navbar").css({"margin-top": "0px", "margin-bottom": "0px"});
+        $(".menu").css("margin-top", 0);
+        $(".logo").css({"height" : 50});
+        $(".menu").css({"background-color": "rgba(0,0,0,1)", "border-bottom": "2px solid rgba(188,209,92,1)"});
+
+      }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })();
